efcea20f2924b5a5448973216b95a6a53423a1b3
blueskyleader01 <blueskyleader01@hotmail.com>
2019-09-11 22:14:50 -0400

    form done

--- Angular/frond-end/src/app/components/home/home.component.ts
+++ Angular/frond-end/src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BikeService } from '../../services/bike.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
 
 
 @Component({
@@ -25,11 +26,28 @@ export class HomeComponent implements OnInit {
       email: new FormControl('', Validators.required),
       phone: new FormControl('', Validators.required),
       model: new FormControl('', Validators.required),
-      serialNumber: new FormControl('', Validators.required), 
+      serialNumber: new FormControl('', Validators.required),
       purchasePrice: new FormControl('', Validators.required),
       purchaseDate: new FormControl('', Validators.required),
       contact: new FormControl()
     })
+  }
+
+  submitRegistration() {
+    if(this.bikeform.valid){
+      this.validMessage = "Your bike registration has been submitted. Thank you";
+      this.bikeService.createBikeRegistration(this.bikeform.value).subscribe(
+        data => {
+          this.bikeform.reset();
+          return true;
+        },
+        error => {
+          return Observable.throw(error)
+        }
+      )
+    } else {
+      this.validMessage = "Please fill out the form before submitted";
+    }
   }
 
 }
