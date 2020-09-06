4a7998d6dfa8fb22d98a20ba51c62268eb184b73
blueskyleader01 <blueskyleader01@hotmail.com>
2020-09-05 22:16:12 -0400

    added form

--- frontend/src/app/services/bike.service.ts
+++ frontend/src/app/services/bike.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http'; 
-import { Observable } from 'rxjs/Observable'; 
+import { Observable } from 'rxjs'; 
 
 const httpOptions = {
   headers: new HttpHeaders({'Content-Type': 'application/json'})
@@ -18,5 +18,13 @@ export class BikeService {
   } //we created proxy.conf.json that redirect my url, so /server/api/v1/bikes will be following http://localhost:4200
   //and the new url is http://localhost:4200/server/api/v1/bikes
 
+  getBike(id: number){
+    return this.http.get(`/server/api/v1/bikes/${id}`);
+  }
+
+  createBikeRegistration(bike){
+    let body = JSON.stringify(bike); 
+    return this.http.post('/server/api/v1/bikes', body, httpOptions)
+  }
 
 }
